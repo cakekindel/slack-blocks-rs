f9fa65593a1de45e8eb58803e3da102adb011348
Orion Kindel <cakekindel@gmail.com>
2020-06-27 15:03:09 -0700

    feat!: remove public things that shouldn't be public (#40)

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,10 @@ extern crate validator_derive;
 pub mod block_elements;
 pub mod blocks;
 pub mod compose;
-pub mod val_helpr;
+mod val_helpr;
 
 #[macro_export]
+#[doc(hidden)]
 macro_rules! impl_from_contents {
     ($enum_name:ident, $variant:ident, $contents_type:ty) => {
         impl From<$contents_type> for $enum_name {
