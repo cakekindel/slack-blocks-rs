e4a72df2c7215859baa08fe0a79120424ed21ef4
Orion Kindel <cakekindel@gmail.com>
2020-06-28 00:58:44 -0700

    add crate doc comment + cargo-readme README generation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+//! [docs 🔗]
+//!
+//! This crate brings Slack's terrific [Block Kit 🔗] to
+//! the Rust ecosystem.
+//!
+//! This crate should hopefully come in handy if you need to
+//! build some rich functionality, or just want to send some
+//! slack messages without having to know Block Kit.
+//!
+//! Inside, you'll find simple models with an API that is
+//! thoroughly documented and (hopefully) easy to use.
+//!
+//! This is currently being actively developed so watch the repo for a
+//! stable v1 release!
+//!
+//! [Block Kit 🔗]: https://api.slack.com/block-kit
+//! [docs 🔗]: https://docs.rs/slack-blocks/latest
+
 #[macro_use]
 extern crate validator_derive;
 
