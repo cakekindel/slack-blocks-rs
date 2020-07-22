4cb2065be859c2a2c3f1b041b2cb8fd5e2e0bb21
Orion Kindel <cakekindel@gmail.com>
2020-07-22 11:01:49 -0700

    feat: confirmation dialog

--- src/compose/mod.rs
+++ src/compose/mod.rs
@@ -7,5 +7,8 @@ pub use opt::Opt;
 pub mod opt_group;
 pub use opt_group::OptGroup;
 
+pub mod confirm;
+pub use confirm::{Confirm, ConfirmStyle};
+
 pub mod conversation_filter;
 pub use conversation_filter::ConversationFilter;
