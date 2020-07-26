e438d0df40c5ed5ed1dc6643ebda4e46b45e9a58
Orion Kindel <cakekindel@gmail.com>
2020-07-26 14:33:05 -0700

    Update slack-dom-to-rustdoc.js

--- .utils/slack-dom-to-rustdoc.js
+++ .utils/slack-dom-to-rustdoc.js
@@ -1,6 +1,9 @@
+// Slack raw HTML string from stdin
+let contents = '';
+
+// Read `contents` from stdin
 process.stdin.setEncoding('utf8');
 
-let contents = '';
 process.stdin.on('readable', () => {
     let chunk = process.stdin.read();
     while (chunk) {
@@ -14,13 +17,28 @@ process.stdin.on('end', () => {
     // functions that the stdin will
     // be piped through
     const transformers = [
-        periodRet,
+        // Separate sentences with newline
+        appendNewlineToPeriods,
+
+        // ex. <a src="/link"> -> <a src="https://api.slack.com/link">
         qualifyRootLinks,
+
+        // ex. <a src="#anchor"> -> <a src="CURRENT_PAGE#anchor">
         qualifyAnchors,
+
+        // ex. <a src="/link">Text</a> -> [Text 🔗] [Text 🔗]: https://api.slack.com/link
         escapeLinks,
-        moveLinkDefs,
+        
+        // Move MD link definitions to the bottom
+        moveLinkDefsToBottom,
+
+        // ex. <code>blah</code> -> `blah`
         escapeCode,
+
+        // ex. <strong>blah</strong> -> **blah**
         escapeStrong,
+
+        // Prepend each line with ///
         docComment,
     ];
 
@@ -30,49 +48,80 @@ process.stdin.on('end', () => {
     process.stdout.write(output);
 });
 
+// Give some meaning to the Array#sort
+// integer return values
+const ord = {
+    aFirst: -1,
+    bFirst: 1,
+    equal: 0,
+};
+
 const helpers = {
+// Test if a string is a markdown link definition,
+// for example: '[Test]: ...' would return true.
+    
+//  isLink :: String -> Boolean
     isLink: str => (/\[.*?\]\:/i).test(str),
-    replace: (pat, replace) => contents => {
+
+// Accepts a Regex pattern, replacement string, and input String
+// Returns the input string with String#replace applied.
+    
+//  replace :: (RegExp, String) -> String -> String
+    replace:   (pat,   replace) => contents => {
         const output = contents.replace(pat, replace);
         return output;
     },
-    eachLine: map => contents => contents.split('\n')
-                                         .map(map)
-                                         .join('\n'),
+
+// Accepts an input String and String -> String fn,
+// Returns the input string with the mapping fn applied to each line.
+    
+//  eachLine :: (String -> String) -> String -> String
+    eachLine:   map                => contents =>
+        contents
+            .split('\n')
+            .map(map)
+            .join('\n'),
 }
 
 // # Transformers
 
-// <a src="/link"> -> <a src="https://api.slack.com/link">
+//    qualifyRootLinks :: String -> String
 const qualifyRootLinks = helpers.replace(
   /<a href\="\/(.*?)">/gi,
   '<a href\="https://api.slack.com/$1">'
 );
 
-// <a src="#anchor"> -> <a src="CURRENT_PAGE#anchor">
+//    qualifyAnchors :: String -> String
 const qualifyAnchors = helpers.replace(
   /<a href\="#(.*?)">/gi,
   `<a href\="CURRENT_PAGE$1">`
 );
 
-// <a src="/link">Text</a> -> [Text 🔗] [Text 🔗]: https://api.slack.com/link
+//    escapeLinks :: String -> String
 const escapeLinks = helpers.replace(
   /<a href\="(.*?)">(.*?)<\/a>/gi,
   '[$2 🔗]%%[$2 🔗]: https://api.slack.com$1\n%%'
 );
 
-// <code>blah</code> -> `blah`
+//    escapeCode :: String -> String
 const escapeCode = helpers.replace(/<code>(.*?)<\/code>/gi, '`$1`');
 
-// <strong>blah</strong> -> **blah**
+//    escapeStrong :: String -> String
 const escapeStrong = helpers.replace(/<strong>(.*?)<\/strong>/gi, '**$1**');
 
-// Separate sentences with newline
-const periodRet = helpers.replace(/\./gi, '.\n');
+//    appendNewlineToPeriods :: String -> String
+const appendNewlineToPeriods = helpers.replace(/\./gi, '.\n');
 
-// Move MD link definitions to the bottom
-const moveLinkDefs = c => c.split(/%%/g).sort((a, b) => helpers.isLink(a) ? 1 : helpers.isLink(b) ? -1 : 0).join('');
+//    moveLinkDefsToBottom :: String -> String
+const moveLinkDefsToBottom = c =>
+    c.split(/%%/g)
+        .sort((a, b) => helpers.isLink(a)
+              ? ord.bFirst
+              : helpers.isLink(b)
+                  ? ord.aFirst
+                  : ord.equal
+        )
+        .join('');
 
-// Prepend each line with ///
+//    docComment :: String -> String
 const docComment = helpers.eachLine(c => '/// ' + c);
-
