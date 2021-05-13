0cbea43d8e89c0926b465e9cbfec1666bf7ab5b7
Orion Kindel <cakekindel@gmail.com>
2021-05-13 14:46:52 -0700

    chore: cargo fmt

--- src/block_elements/select/builder.rs
+++ src/block_elements/select/builder.rs
@@ -1,224 +1,218 @@
 use std::borrow::Cow;
 
-use crate::compose::Confirm;
-use crate::text;
-
-use super::Conversation;
-use super::External;
-use super::PublicChannel;
-use super::User;
+use super::{Conversation, External, PublicChannel, User};
+use crate::{compose::Confirm, text};
 
 /// # Select Element Builder
 /// Use to construct a Select element
 /// and easily choose a data source
 pub struct SelectBuilder<'a> {
-    pub placeholder: text::Plain,
-    pub action_id: Cow<'a, str>,
-    pub confirm: Option<Confirm>,
+  pub placeholder: text::Plain,
+  pub action_id: Cow<'a, str>,
+  pub confirm: Option<Confirm>,
 }
 
 impl<'a> SelectBuilder<'a> {
-    /// Construct a Select block element from required parts
-    ///
-    /// # Arguments
-    /// - `placeholder`: A [text object 🔗]
-    ///
-    /// [text objects 🔗]: https://api.slack.com/reference/messaging/composition-objects#text
-    ///
-    /// # Example
-    /// See example for `Select::from_placeholder_and_action_id`
-    pub fn from_placeholder_and_action_id(
-        placeholder: impl Into<text::Plain>,
-        action_id: impl Into<Cow<'a, str>>,
-    ) -> Self {
-        Self {
-            placeholder: placeholder.into(),
-            action_id: action_id.into(),
-            confirm: None,
-        }
+  /// Construct a Select block element from required parts
+  ///
+  /// # Arguments
+  /// - `placeholder`: A [text object 🔗]
+  ///
+  /// [text objects 🔗]: https://api.slack.com/reference/messaging/composition-objects#text
+  ///
+  /// # Example
+  /// See example for `Select::from_placeholder_and_action_id`
+  pub fn from_placeholder_and_action_id(placeholder: impl Into<text::Plain>,
+                                        action_id: impl Into<Cow<'a, str>>)
+                                        -> Self {
+    Self { placeholder: placeholder.into(),
+           action_id: action_id.into(),
+           confirm: None }
+  }
+
+  /// Construct a Select block element from required parts
+  ///
+  /// # Arguments
+  /// - `confirm`: A [confirm object 🔗] that defines an optional confirmation dialog that appears after a menu item is selected.
+  ///
+  /// [confirm object 🔗]: https://api.slack.com/reference/block-kit/composition-objects#confirm
+  ///
+  /// # Example
+  /// ```
+  /// use std::iter;
+  /// use std::convert::TryFrom;
+  ///
+  /// use slack_blocks::{
+  ///   blocks::{Block, Actions},
+  ///   block_elements::{BlockElement, select::Select},
+  ///   compose::{text, Confirm, text::ToSlackPlaintext},
+  /// };
+  ///
+  /// # use std::error::Error;
+  /// # pub fn main() -> Result<(), Box<dyn Error>> {
+  ///
+  /// let confirm = Confirm::from_parts(
+  ///   "Are you sure?",
+  ///   "Think hard about this.".plaintext(),
+  ///   "Yes",
+  ///   "No",
+  /// );
+  ///
+  /// let select: BlockElement = Select::from_placeholder_and_action_id("Pick a channel to delete!", "delete_chan_select")
+  ///                                   .with_confirm(confirm)
+  ///                                   .choose_from_public_channels()
+  ///                                   .into();
+  ///
+  /// let block: Block = Actions::try_from(select).unwrap().into();
+  ///
+  /// // < send `block` to slack API >
+  /// # Ok(())
+  /// # }
+  /// ```
+  pub fn with_confirm(mut self, confirm: Confirm) -> Self {
+    self.confirm = Some(confirm);
+    self
+  }
+
+  /// Turn the builder into a Public Channel select element
+  ///
+  /// # Example
+  /// ```
+  /// use std::iter;
+  /// use std::convert::TryFrom;
+  ///
+  /// use slack_blocks::{
+  ///   blocks::{Block, Section, Actions},
+  ///   block_elements::{BlockElement, select::Select},
+  ///   compose::{text, Confirm},
+  /// };
+  ///
+  /// # use std::error::Error;
+  /// # pub fn main() -> Result<(), Box<dyn Error>> {
+  ///
+  /// let confirm = Confirm::from_parts("Are you sure?", text::Mrkdwn::from("Think hard about this."), "Yes", "No");
+  ///
+  /// let select: BlockElement = Select::from_placeholder_and_action_id("Pick a channel to delete!", "1234")
+  ///                                   .with_confirm(confirm)
+  ///                                   .choose_from_public_channels()
+  ///                                   .into();
+  ///
+  /// let block: Block = Actions::try_from(select).unwrap().into();
+  ///
+  /// // < send `block` to slack API >
+  /// # Ok(())
+  /// # }
+  /// ```
+  pub fn choose_from_public_channels(self) -> PublicChannel<'a> {
+    let sel = PublicChannel::from_placeholder_and_action_id(self.placeholder,
+                                                            self.action_id);
+
+    match self.confirm {
+      | Some(confirm) => sel.with_confirm(confirm),
+      | None => sel,
     }
+  }
 
-    /// Construct a Select block element from required parts
-    ///
-    /// # Arguments
-    /// - `confirm`: A [confirm object 🔗] that defines an optional confirmation dialog that appears after a menu item is selected.
-    ///
-    /// [confirm object 🔗]: https://api.slack.com/reference/block-kit/composition-objects#confirm
-    ///
-    /// # Example
-    /// ```
-    /// use std::iter;
-    /// use std::convert::TryFrom;
-    ///
-    /// use slack_blocks::{
-    ///   blocks::{Block, Actions},
-    ///   block_elements::{BlockElement, select::Select},
-    ///   compose::{text, Confirm, text::ToSlackPlaintext},
-    /// };
-    ///
-    /// # use std::error::Error;
-    /// # pub fn main() -> Result<(), Box<dyn Error>> {
-    ///
-    /// let confirm = Confirm::from_parts(
-    ///   "Are you sure?",
-    ///   "Think hard about this.".plaintext(),
-    ///   "Yes",
-    ///   "No",
-    /// );
-    ///
-    /// let select: BlockElement = Select::from_placeholder_and_action_id("Pick a channel to delete!", "delete_chan_select")
-    ///                                   .with_confirm(confirm)
-    ///                                   .choose_from_public_channels()
-    ///                                   .into();
-    ///
-    /// let block: Block = Actions::try_from(select).unwrap().into();
-    ///
-    /// // < send `block` to slack API >
-    /// # Ok(())
-    /// # }
-    /// ```
-    pub fn with_confirm(mut self, confirm: Confirm) -> Self {
-        self.confirm = Some(confirm);
-        self
+  /// Set the data source to "All conversations". See docs for `select::Conversation` for more info.
+  ///
+  /// # Example
+  /// ```
+  /// use std::iter;
+  /// use std::convert::TryFrom;
+  ///
+  /// use slack_blocks::{
+  ///   blocks::{Block, Section, Actions},
+  ///   block_elements::{BlockElement, select::Select},
+  ///   compose::{text, Confirm},
+  /// };
+  ///
+  /// use text::ToSlackPlaintext;
+  ///
+  /// # use std::error::Error;
+  /// # pub fn main() -> Result<(), Box<dyn Error>> {
+  ///
+  /// let select: BlockElement = Select::from_placeholder_and_action_id("Channel", "get_member_channel_select")
+  ///                                   .choose_from_all_channels()
+  ///                                   .into();
+  ///
+  /// let blocks: Vec<Block> = vec![
+  ///   Section::from_text("Get the members of a channel".plaintext()).into(),
+  ///   Actions::try_from(select).unwrap().into(),
+  /// ];
+  ///
+  /// // <send `blocks` to slack's API>
+  /// # Ok(())
+  /// # }
+  /// ```
+  pub fn choose_from_all_channels(self) -> Conversation<'a> {
+    let sel = Conversation::from_placeholder_and_action_id(self.placeholder,
+                                                           self.action_id);
+
+    match self.confirm {
+      | Some(confirm) => sel.with_confirm(confirm),
+      | None => sel,
     }
+  }
 
-    /// Turn the builder into a Public Channel select element
-    ///
-    /// # Example
-    /// ```
-    /// use std::iter;
-    /// use std::convert::TryFrom;
-    ///
-    /// use slack_blocks::{
-    ///   blocks::{Block, Section, Actions},
-    ///   block_elements::{BlockElement, select::Select},
-    ///   compose::{text, Confirm},
-    /// };
-    ///
-    /// # use std::error::Error;
-    /// # pub fn main() -> Result<(), Box<dyn Error>> {
-    ///
-    /// let confirm = Confirm::from_parts("Are you sure?", text::Mrkdwn::from("Think hard about this."), "Yes", "No");
-    ///
-    /// let select: BlockElement = Select::from_placeholder_and_action_id("Pick a channel to delete!", "1234")
-    ///                                   .with_confirm(confirm)
-    ///                                   .choose_from_public_channels()
-    ///                                   .into();
-    ///
-    /// let block: Block = Actions::try_from(select).unwrap().into();
-    ///
-    /// // < send `block` to slack API >
-    /// # Ok(())
-    /// # }
-    /// ```
-    pub fn choose_from_public_channels(self) -> PublicChannel<'a> {
-        let sel = PublicChannel::from_placeholder_and_action_id(self.placeholder, self.action_id);
+  /// Set the data source to "users". See docs for `select::User` for more info.
+  ///
+  /// # Example
+  /// ```
+  /// use std::{convert::TryFrom, iter};
+  ///
+  /// use slack_blocks::{block_elements::{select::Select, BlockElement},
+  ///                    blocks::{Actions, Block, Section},
+  ///                    text,
+  ///                    text::ToSlackPlaintext};
+  ///
+  /// let select: BlockElement =
+  ///   Select::from_placeholder_and_action_id("Choose a user to ban!!",
+  ///                                          "ban_chosen").choose_from_users()
+  ///                                                       .into();
+  ///
+  /// let blocks: Block = Actions::try_from(select).unwrap().into();
+  ///
+  /// // <send `blocks` to slack's API>
+  /// ```
+  pub fn choose_from_users(self) -> User<'a> {
+    let sel =
+      User::from_placeholder_and_action_id(self.placeholder, self.action_id);
 
-        match self.confirm {
-            Some(confirm) => sel.with_confirm(confirm),
-            None => sel,
-        }
+    match self.confirm {
+      | Some(confirm) => sel.with_confirm(confirm),
+      | None => sel,
     }
+  }
 
-    /// Set the data source to "All conversations". See docs for `select::Conversation` for more info.
-    ///
-    /// # Example
-    /// ```
-    /// use std::iter;
-    /// use std::convert::TryFrom;
-    ///
-    /// use slack_blocks::{
-    ///   blocks::{Block, Section, Actions},
-    ///   block_elements::{BlockElement, select::Select},
-    ///   compose::{text, Confirm},
-    /// };
-    ///
-    /// use text::ToSlackPlaintext;
-    ///
-    /// # use std::error::Error;
-    /// # pub fn main() -> Result<(), Box<dyn Error>> {
-    ///
-    /// let select: BlockElement = Select::from_placeholder_and_action_id("Channel", "get_member_channel_select")
-    ///                                   .choose_from_all_channels()
-    ///                                   .into();
-    ///
-    /// let blocks: Vec<Block> = vec![
-    ///   Section::from_text("Get the members of a channel".plaintext()).into(),
-    ///   Actions::try_from(select).unwrap().into(),
-    /// ];
-    ///
-    /// // <send `blocks` to slack's API>
-    /// # Ok(())
-    /// # }
-    /// ```
-    pub fn choose_from_all_channels(self) -> Conversation<'a> {
-        let sel = Conversation::from_placeholder_and_action_id(self.placeholder, self.action_id);
+  /// Users will choose from an external data source. See docs for `select::External` for more info.
+  ///
+  /// # Example
+  /// ```
+  /// use std::iter;
+  /// use std::convert::TryFrom;
+  ///
+  /// use slack_blocks::{
+  ///   text,
+  ///   text::ToSlackPlaintext,
+  ///   blocks::{Block, Section, Actions},
+  ///   block_elements::{BlockElement, select::Select},
+  /// };
+  ///
+  /// let select: BlockElement = Select::from_placeholder_and_action_id("Pick your favorite cheese", "cheese_chosen")
+  ///                                   .choose_from_external()
+  ///                                   .into();
+  ///
+  /// let blocks: Block = Actions::try_from(select).unwrap().into();
+  ///
+  /// // <send `blocks` to slack's API>
+  /// ```
+  pub fn choose_from_external(self) -> External<'a> {
+    let sel = External::from_placeholder_and_action_id(self.placeholder,
+                                                       self.action_id);
 
-        match self.confirm {
-            Some(confirm) => sel.with_confirm(confirm),
-            None => sel,
-        }
+    match self.confirm {
+      | Some(confirm) => sel.with_confirm(confirm),
+      | None => sel,
     }
-
-    /// Set the data source to "users". See docs for `select::User` for more info.
-    ///
-    /// # Example
-    /// ```
-    /// use std::iter;
-    /// use std::convert::TryFrom;
-    ///
-    /// use slack_blocks::{
-    ///   text,
-    ///   text::ToSlackPlaintext,
-    ///   blocks::{Block, Section, Actions},
-    ///   block_elements::{BlockElement, select::Select},
-    /// };
-    ///
-    /// let select: BlockElement = Select::from_placeholder_and_action_id("Choose a user to ban!!", "ban_chosen")
-    ///                                   .choose_from_users()
-    ///                                   .into();
-    ///
-    /// let blocks: Block = Actions::try_from(select).unwrap().into();
-    ///
-    /// // <send `blocks` to slack's API>
-    /// ```
-    pub fn choose_from_users(self) -> User<'a> {
-        let sel = User::from_placeholder_and_action_id(self.placeholder, self.action_id);
-
-        match self.confirm {
-            Some(confirm) => sel.with_confirm(confirm),
-            None => sel,
-        }
-    }
-
-    /// Users will choose from an external data source. See docs for `select::External` for more info.
-    ///
-    /// # Example
-    /// ```
-    /// use std::iter;
-    /// use std::convert::TryFrom;
-    ///
-    /// use slack_blocks::{
-    ///   text,
-    ///   text::ToSlackPlaintext,
-    ///   blocks::{Block, Section, Actions},
-    ///   block_elements::{BlockElement, select::Select},
-    /// };
-    ///
-    /// let select: BlockElement = Select::from_placeholder_and_action_id("Pick your favorite cheese", "cheese_chosen")
-    ///                                   .choose_from_external()
-    ///                                   .into();
-    ///
-    /// let blocks: Block = Actions::try_from(select).unwrap().into();
-    ///
-    /// // <send `blocks` to slack's API>
-    /// ```
-    pub fn choose_from_external(self) -> External<'a> {
-        let sel = External::from_placeholder_and_action_id(self.placeholder, self.action_id);
-
-        match self.confirm {
-            Some(confirm) => sel.with_confirm(confirm),
-            None => sel,
-        }
-    }
+  }
 }
--- src/blocks/context.rs
+++ src/blocks/context.rs
@@ -1,9 +1,7 @@
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
-use crate::convert;
-use crate::text;
-use crate::val_helpr::ValidationResult;
+use crate::{convert, text, val_helpr::ValidationResult};
 
 /// # Context Block
 ///
@@ -12,177 +10,176 @@ use crate::val_helpr::ValidationResult;
 /// Displays message context, which can include both images and text.
 ///
 /// [context_docs]: https://api.slack.com/reference/block-kit/blocks#context
-#[derive(Clone, Debug, Default, Deserialize, Hash, PartialEq, Serialize, Validate)]
+#[derive(Clone,
+           Debug,
+           Default,
+           Deserialize,
+           Hash,
+           PartialEq,
+           Serialize,
+           Validate)]
 pub struct Contents {
-    #[validate(length(max = 10))]
-    elements: Vec<Compose>,
+  #[validate(length(max = 10))]
+  elements: Vec<Compose>,
 
-    #[serde(skip_serializing_if = "Option::is_none")]
-    #[validate(length(max = 255))]
-    block_id: Option<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  #[validate(length(max = 255))]
+  block_id: Option<String>,
 }
 
 impl Contents {
-    /// Create an empty Context block (shorthand for `Default::default()`)
-    ///
-    /// # Example
-    /// ```
-    /// use slack_blocks::blocks::{Block, context};
-    /// use slack_blocks::text;
-    ///
-    /// let context = context::Contents::new()
-    ///     .with_element(text::Plain::from("my unformatted text"));
-    ///
-    /// let block: Block = context.into();
-    /// // < send block to slack's API >
-    /// ```
-    pub fn new() -> Self {
-        Default::default()
-    }
+  /// Create an empty Context block (shorthand for `Default::default()`)
+  ///
+  /// # Example
+  /// ```
+  /// use slack_blocks::{blocks::{context, Block},
+  ///                    text};
+  ///
+  /// let context = context::Contents::new()
+  ///     .with_element(text::Plain::from("my unformatted text"));
+  ///
+  /// let block: Block = context.into();
+  /// // < send block to slack's API >
+  /// ```
+  pub fn new() -> Self {
+    Default::default()
+  }
 
-    /// Set the `block_id` for interactions on an existing `context::Contents`
-    ///
-    /// # Arguments
-    /// - `block_id` - A string acting as a unique identifier for a block.
-    ///     You can use this `block_id` when you receive an interaction payload
-    ///     to [identify the source of the action 🔗].
-    ///     If not specified, a `block_id` will be generated.
-    ///     Maximum length for this field is 255 characters.
-    ///
-    /// [identify the source of the action 🔗]: https://api.slack.com/interactivity/handling#payloads
-    ///
-    /// # Example
-    /// ```
-    /// use slack_blocks::blocks::{Block, context};
-    /// use slack_blocks::text;
-    ///
-    /// let text = text::Mrkdwn::from("_flavor_ *text*");
-    /// let context: Block = context::Contents::new()
-    ///     .with_element(text)
-    ///     .with_block_id("msg_id_12346")
-    ///     .into();
-    ///
-    /// // < send block to slack's API >
-    /// ```
-    pub fn with_block_id(mut self, block_id: impl ToString) -> Self {
-        self.block_id = Some(block_id.to_string());
-        self
-    }
+  /// Set the `block_id` for interactions on an existing `context::Contents`
+  ///
+  /// # Arguments
+  /// - `block_id` - A string acting as a unique identifier for a block.
+  ///     You can use this `block_id` when you receive an interaction payload
+  ///     to [identify the source of the action 🔗].
+  ///     If not specified, a `block_id` will be generated.
+  ///     Maximum length for this field is 255 characters.
+  ///
+  /// [identify the source of the action 🔗]: https://api.slack.com/interactivity/handling#payloads
+  ///
+  /// # Example
+  /// ```
+  /// use slack_blocks::{blocks::{context, Block},
+  ///                    text};
+  ///
+  /// let text = text::Mrkdwn::from("_flavor_ *text*");
+  /// let context: Block = context::Contents::new().with_element(text)
+  ///                                              .with_block_id("msg_id_12346")
+  ///                                              .into();
+  ///
+  /// // < send block to slack's API >
+  /// ```
+  pub fn with_block_id(mut self, block_id: impl ToString) -> Self {
+    self.block_id = Some(block_id.to_string());
+    self
+  }
 
-    /// Add a composition object to a context block.
-    ///
-    /// This is chainable, and can be used to easily
-    /// populate the elements of a context block
-    /// right after invoking `new`.
-    ///
-    /// # Arguments
-    /// - `element` - A composition object;
-    ///     Must be image elements or text objects.
-    ///     Maximum number of items is 10.
-    ///
-    /// # Example
-    /// ```
-    /// use slack_blocks::blocks::{Block, context};
-    /// use slack_blocks::text;
-    ///
-    /// let context = context::Contents::new()
-    ///     .with_element(text::Plain::from("my unformatted text"));
-    ///
-    /// let block: Block = context.into();
-    /// // < send block to slack's API >
-    /// ```
-    pub fn with_element(mut self, element: impl Into<self::Compose>) -> Self {
-        self.elements.push(element.into());
-        self
-    }
+  /// Add a composition object to a context block.
+  ///
+  /// This is chainable, and can be used to easily
+  /// populate the elements of a context block
+  /// right after invoking `new`.
+  ///
+  /// # Arguments
+  /// - `element` - A composition object;
+  ///     Must be image elements or text objects.
+  ///     Maximum number of items is 10.
+  ///
+  /// # Example
+  /// ```
+  /// use slack_blocks::{blocks::{context, Block},
+  ///                    text};
+  ///
+  /// let context = context::Contents::new()
+  ///     .with_element(text::Plain::from("my unformatted text"));
+  ///
+  /// let block: Block = context.into();
+  /// // < send block to slack's API >
+  /// ```
+  pub fn with_element(mut self, element: impl Into<self::Compose>) -> Self {
+    self.elements.push(element.into());
+    self
+  }
 
-    /// Construct a new `context::Contents` from a collection of
-    /// composition objects that are may not be supported by Context
-    /// Blocks.
-    ///
-    /// If you _can't_ guarantee that a collection only contains image
-    /// or text objects, `from_elements` may be more ergonomic for you.
-    ///
-    /// # Arguments
-    /// - `elements` - An array of composition objects;
-    ///     Must be image elements or text objects.
-    ///     Maximum number of items is 10.
-    ///
-    /// # Examples
-    /// ```
-    /// use slack_blocks::blocks::{Block, context};
-    /// use slack_blocks::text;
-    ///
-    /// pub fn main() {
-    ///     let objs: Vec<text::Mrkdwn> = vec![
-    ///         text::Mrkdwn::from("*s i c k*"),
-    ///         text::Mrkdwn::from("*t i g h t*"),
-    ///     ];
-    ///     let context = context::Contents::from_context_elements(objs);
-    ///     let block: Block = context.into();
-    ///     // < send block to slack's API >
-    /// }
-    /// ```
-    pub fn from_context_elements(elements: impl IntoIterator<Item = impl Into<Compose>>) -> Self {
-        elements
-            .into_iter()
+  /// Construct a new `context::Contents` from a collection of
+  /// composition objects that are may not be supported by Context
+  /// Blocks.
+  ///
+  /// If you _can't_ guarantee that a collection only contains image
+  /// or text objects, `from_elements` may be more ergonomic for you.
+  ///
+  /// # Arguments
+  /// - `elements` - An array of composition objects;
+  ///     Must be image elements or text objects.
+  ///     Maximum number of items is 10.
+  ///
+  /// # Examples
+  /// ```
+  /// use slack_blocks::{blocks::{context, Block},
+  ///                    text};
+  ///
+  /// pub fn main() {
+  ///   let objs: Vec<text::Mrkdwn> = vec![text::Mrkdwn::from("*s i c k*"),
+  ///                                      text::Mrkdwn::from("*t i g h t*"),];
+  ///   let context = context::Contents::from_context_elements(objs);
+  ///   let block: Block = context.into();
+  ///   // < send block to slack's API >
+  /// }
+  /// ```
+  pub fn from_context_elements(elements: impl IntoIterator<Item = impl Into<Compose>>)
+                               -> Self {
+    elements.into_iter()
             .map(Into::<self::Compose>::into)
             .collect::<Vec<_>>()
             .into()
-    }
+  }
 
-    /// Validate that this Context block agrees with Slack's model requirements
-    ///
-    /// # Errors
-    /// - If `with_block_id` was called with a block id longer
-    ///     than 255 chars
-    /// - If `from_elements`, `from_context_elements`, or `with_element` was called with
-    ///     more than 10 objects
-    ///
-    /// # Example
-    /// ```
-    /// use slack_blocks::blocks;
-    ///
-    /// let long_string = std::iter::repeat(' ').take(256).collect::<String>();
-    ///
-    /// let block = blocks::context
-    ///     ::Contents
-    ///     ::new()
-    ///     .with_block_id(long_string);
-    ///
-    /// assert_eq!(true, matches!(block.validate(), Err(_)));
-    /// ```
-    pub fn validate(&self) -> ValidationResult {
-        Validate::validate(self)
-    }
+  /// Validate that this Context block agrees with Slack's model requirements
+  ///
+  /// # Errors
+  /// - If `with_block_id` was called with a block id longer
+  ///     than 255 chars
+  /// - If `from_elements`, `from_context_elements`, or `with_element` was called with
+  ///     more than 10 objects
+  ///
+  /// # Example
+  /// ```
+  /// use slack_blocks::blocks;
+  ///
+  /// let long_string = std::iter::repeat(' ').take(256).collect::<String>();
+  ///
+  /// let block = blocks::context::Contents::new().with_block_id(long_string);
+  ///
+  /// assert_eq!(true, matches!(block.validate(), Err(_)));
+  /// ```
+  pub fn validate(&self) -> ValidationResult {
+    Validate::validate(self)
+  }
 }
 
 impl From<Vec<Compose>> for Contents {
-    fn from(elements: Vec<Compose>) -> Self {
-        Self {
-            elements,
-            ..Default::default()
-        }
-    }
+  fn from(elements: Vec<Compose>) -> Self {
+    Self { elements,
+           ..Default::default() }
+  }
 }
 
 /// The Composition objects supported by this block
 #[derive(Clone, Debug, Deserialize, Hash, PartialEq, Serialize)]
 pub enum Compose {
-    Text(text::Text),
-    Image,
+  Text(text::Text),
+  Image,
 }
 
 convert!(impl From<text::Text> for Compose => |txt| Compose::Text(txt));
 
 impl From<text::plain::Contents> for Compose {
-    fn from(text: text::plain::Contents) -> Self {
-        Into::<text::Text>::into(text).into()
-    }
+  fn from(text: text::plain::Contents) -> Self {
+    Into::<text::Text>::into(text).into()
+  }
 }
 
 impl From<text::Mrkdwn> for Compose {
-    fn from(text: text::Mrkdwn) -> Self {
-        Into::<text::Text>::into(text).into()
-    }
+  fn from(text: text::Mrkdwn) -> Self {
+    Into::<text::Text>::into(text).into()
+  }
 }
