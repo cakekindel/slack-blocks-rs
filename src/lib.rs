79282c5272b27f2493b39d011946c7d026246842
Orion Kindel <cakekindel@gmail.com>
2021-05-14 11:01:44 -0700

    ci: add cargo-make for consistency (#119)

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,15 @@
 //!
 //! [Block Kit 🔗]: https://api.slack.com/block-kit
 //!
+//! # Build / Test / Format
+//! This crate uses [`cargo-make`] for script consistency, in Makefile.toml you'll find:
+//!   - `cargo make fmt`: Format all files according to configured style `rustfmt.toml`
+//!   - `cargo make test`: Run all tests
+//!   - `cargo make doctest`: Run doc tests only
+//!   - `cargo make tdd`: Watch files for changes, and run `cargo make test` on each change
+//!   - `cargo make ci`: Run tests, check that code is formatted and no lint violations.
+//!                      This is run as a quality gate for all pull requests.
+//!
 //! # Contributing
 //!
 //! If you're interested in contributing, head over to the [issues] and see what's left to
@@ -32,6 +41,7 @@
 //! but you don't _need_ to follow this convention since the repo is configured for Squash
 //! commits on merge.
 //!
+//! [`cargo-make`]: https://github.com/sagiegurari/cargo-make/
 //! [issues]: https://github.com/cakekindel/slack-blocks-rs/issues/
 //! [Conventional Commits]: https://www.conventionalcommits.org/en/v1.0.0/
 
